01fd512adb5e44a10746b96f8a5ae71033c858a9
PaulBS-glitch <junkvitto@gmail.com>
2026-05-22 15:11:16 +1000

    Bump responsive cache after split hero stabilisation

--- roadmap-data.js
+++ roadmap-data.js
@@ -25,7 +25,7 @@
     const link = document.createElement("link");
     link.id = "roadmap-responsive-style";
     link.rel = "stylesheet";
-    link.href = "roadmap-responsive.css?v=20260521-44";
+    link.href = "roadmap-responsive.css?v=20260521-45";
     document.head.appendChild(link);
   }
 
