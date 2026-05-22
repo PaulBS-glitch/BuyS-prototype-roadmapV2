c02597011ba25af47ae7fb5d2412a239b22a20aa
PaulBS-glitch <junkvitto@gmail.com>
2026-05-22 14:45:10 +1000

    Bump responsive stylesheet cache

--- roadmap-data.js
+++ roadmap-data.js
@@ -25,7 +25,7 @@
     const link = document.createElement("link");
     link.id = "roadmap-responsive-style";
     link.rel = "stylesheet";
-    link.href = "roadmap-responsive.css?v=20260521-41";
+    link.href = "roadmap-responsive.css?v=20260521-42";
     document.head.appendChild(link);
   }
 
